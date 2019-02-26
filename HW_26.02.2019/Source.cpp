7b2022c392de782ac7fe88b94c8848e45ace5461
dinara0708 <aikenova01@gmail.com>
2019-02-26 23:14:17 +0600

    -

--- HW_26.02.2019/Source.cpp
+++ HW_26.02.2019/Source.cpp
@@ -5,17 +5,53 @@
 
 using namespace std;
 
+// 1.	**Реализовать 3 функции каждая из которых принимает вещественное число и возвращает вещественное число.
+// Первая функция вычисляет квадратный корень числа, вторая – куб числа, третья – синус числа.
+// В главной функции создайте массив из 3 - х указателей на эти функции.
+// Добавьте меню, в котором пользователь может выбрать желаемую операцию.
+// Напишите программу без использования операторов if и switch.
+
+double sq(double x) {
+	double y = sqrt(x);
+	return y;
+}
+double cube(double x) {
+	double y = x * x*x;
+	return y;
+}
+double sinus(double x) {
+	double y = sin(x);
+	return y;
+}
+
 int main()
 {
 	srand(time(NULL));
 
 	setlocale(LC_ALL, "Rus");
 
+	double(*functions[3])(double) = { sq, cube, sinus };
+	int choice, number ;
 
 	while (true)
 	{
+		cout << "Введите 0, чтобы получить квадратный корень числа" << endl;
+		cout << "Введите 1, чтобы получить куб числа" << endl;
+		cout << "Введите 2, чтобы получить синус числа" << endl;
+		cout << "Введите -1 для выхода" << endl;
+		cin >> choice;
 
+		if (choice == -1) break;
+		else if (choice < 0 || choice >2) cout << "Try again!" << endl;
+		else
+		{
+			cout << "Enter a number" << endl;
+			cin >> number;
+			cout << functions[choice](number) << endl;
+		}
 	}
+
+
 	system("pause");
 	return 0;
 }
