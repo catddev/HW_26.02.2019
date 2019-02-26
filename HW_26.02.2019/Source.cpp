41f91641ee1c8e3111597f9eab56ec823618fc67
dinara0708 <aikenova01@gmail.com>
2019-02-26 22:55:39 +0600

    Добавьте файлы проекта.

---
+++ HW_26.02.2019/Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iomanip>
+#include<math.h>
+#include<ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+
+	while (true)
+	{
+
+	}
+	system("pause");
+	return 0;
+}
